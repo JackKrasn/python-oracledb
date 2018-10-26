31f2efa48a7c1466bb12123aaa00e8d3256e3423
Krasnukhin Evgeniy <e.krasnukhin@ftc.ru>
2018-10-26 12:32:56 +0700

    версия 1.0.3

--- oracledb/__init__.py
+++ oracledb/__init__.py
@@ -3,7 +3,7 @@ from .db import *
 from .dbexceptions import *
 from .orautils import *
 
-__version__ = '1.0.2'  # https://semver.org/lang/ru/
+__version__ = '1.0.3'  # https://semver.org/lang/ru/
 __author__ = 'Evgeniy Krasnukhin'
 __author_email__ = 'e.krasnukhin@cft.ru'
 
